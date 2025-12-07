5e750356c8854e831fe02eeb5fc32ea32ad716d1
Waheed Ahmad <waheedahmad5519@gmail.com>
2025-12-07 21:10:49 +0500

    model updated

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if not api_key:
     st.stop()
 
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 # Streamlit UI
 st.set_page_config(page_title="Professional README Generator", page_icon="📝")
